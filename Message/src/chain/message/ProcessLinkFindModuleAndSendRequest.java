96d5fc51ad7783fe1bdf1bda8cb7cf6cd6220b76
thanhdo <billslim0996@gmail.com>
2021-11-19 16:46:36 +0700

    visually change for easier time to read

--- Message/src/chain/message/ProcessLinkFindModuleAndSendRequest.java
+++ Message/src/chain/message/ProcessLinkFindModuleAndSendRequest.java
@@ -2,6 +2,7 @@ package chain.message;
 
 import chain.Link;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import connection_and_storage.connection.listener.Listener;
 import connection_and_storage.connection.socket.PlainSocket;
 import memorable.MemorableMessage;
@@ -15,18 +16,15 @@ public class ProcessLinkFindModuleAndSendRequest extends Link<ProcessChain> {
 
     @Override
     protected boolean resolve() {
-        JsonArray toJsonArray = chain.getProcessObject().get("header").getAsJsonObject()
-                .get("to").getAsJsonArray();
-        boolean requestStatus = chain.getProcessObject().get("header").getAsJsonObject()
-                .get("status").getAsBoolean();
+        JsonObject headerObject = chain.getProcessObject().get("header").getAsJsonObject();
+        JsonArray toJsonArray = headerObject.get("to").getAsJsonArray();
+        boolean requestStatus = headerObject.get("status").getAsBoolean();
         // the request return to the original module when the error flag is raise or the chain of modules is completed
         Listener<PlainSocket> listener = MemorableMessage.getListener();
         PlainSocket receiveInfoSocket;
         if (toJsonArray.size() == 0 || !requestStatus) { // the module reach the end of its chain or already failed to resolve
-            String fromModule = chain.getProcessObject().get("header").getAsJsonObject()
-                    .get("from").getAsString();
-            int hashCode = chain.getProcessObject().get("header").getAsJsonObject()
-                    .get("hashCode").getAsInt();
+            String fromModule = headerObject.get("from").getAsString();
+            int hashCode = headerObject.get("hashCode").getAsInt();
             try {
                 receiveInfoSocket = listener.get(fromModule, hashCode);
             } catch (IllegalAccessException e) {
@@ -36,17 +34,19 @@ public class ProcessLinkFindModuleAndSendRequest extends Link<ProcessChain> {
                 return false; // this wouldn't run, but it is required for the variable to be registered
             }
         } else { // the module still in the middle of the process and still valid
-            String to = chain.getProcessObject().get("header").getAsJsonObject()
+            String to = headerObject
                     .get("to").getAsJsonArray()
                     .remove(0).getAsString();
             receiveInfoSocket = listener.get(to);
         }
+        //send the message to the indicated module
         try {
             receiveInfoSocket.write(chain.getProcessObject().toString());
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
+        // increase the counter of the module
         receiveInfoSocket.increaseCounter();
         return true;
     }
