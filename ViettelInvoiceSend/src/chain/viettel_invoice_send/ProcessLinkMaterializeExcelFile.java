a7827b61c41c45dc6a63f138fb4979b292ef9caa
thanhdo <billslim0996@gmail.com>
2021-11-22 17:26:56 +0700

    convert file form encoded base-64 to an actual file

---
+++ ViettelInvoiceSend/src/chain/viettel_invoice_send/ProcessLinkMaterializeExcelFile.java
@@ -0,0 +1,59 @@
+package chain.viettel_invoice_send;
+
+import chain.Link;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class ProcessLinkMaterializeExcelFile extends Link<ProcessChain> {
+    public ProcessLinkMaterializeExcelFile(ProcessChain chain) {
+        super(chain);
+    }
+
+    /**
+     * convert the Excel file send to the server into file to actually read it
+     *
+     * @return <code>true</code> when successfully run this block code
+     * <code>false</code> when unsuccessfully run this block code
+     */
+    @Override
+    protected boolean resolve() {
+        // get the encoded file within the body then decode it
+        byte[] encodedByteArray, decodedByteArray;
+        try {
+            encodedByteArray = chain.getProcessObject().get("body").getAsJsonObject()
+                    .get("file").getAsString()
+                    .getBytes(StandardCharsets.UTF_8);
+        } catch (NullPointerException e) {
+            chain.getProcessObject().get("body").getAsJsonObject()
+                    .addProperty("response","Thông tin gởi đi thiếu tệp tin excel đính kèm");
+            System.err.println("Missing the file object within the body request");
+            e.printStackTrace();
+            return false;
+        }
+        decodedByteArray = Base64.getDecoder().decode(encodedByteArray);
+        // create file to hold the data
+        File excelFile = new File(Thread.currentThread().getName());
+        // write the decodedByteArray into the file
+        try {
+            FileOutputStream fos = new FileOutputStream(excelFile);
+            fos.write(decodedByteArray);
+            fos.close();
+        } catch (IOException e) {
+            chain.getProcessObject().get("body").getAsJsonObject()
+                    .addProperty("response", "Không thể ghi tệp tin vui lòng liên hệ công ty phần mềm");
+            System.out.println("Cannot write the file down");
+            e.printStackTrace();
+            return false;
+        }
+        // remove after done using
+        this.chain.getProcessObject().get("body").getAsJsonObject()
+                .remove("file");
+
+        this.chain.setExcelFile(excelFile);
+        return true;
+    }
+}
