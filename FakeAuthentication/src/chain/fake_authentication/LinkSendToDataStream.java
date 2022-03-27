2cd1201fa2cee3b914a3fee1dcf71fbf53b7e717
thanhdo <billslim0996@gmail.com>
2022-03-27 01:41:41 -0700

    write json back to the data stream after process

---
+++ FakeAuthentication/src/chain/fake_authentication/LinkSendToDataStream.java
@@ -0,0 +1,33 @@
+package chain.fake_authentication;
+
+import chain.Chain;
+import chain.Link;
+import memorable.FakeAuthentication;
+import socket.Socket;
+
+import java.io.IOException;
+
+public class LinkSendToDataStream extends Link {
+    public LinkSendToDataStream(Chain chain) {
+        super(chain);
+    }
+
+    /**
+     * resolve the request within this chain
+     *
+     * @return <code>true</code> when successfully run this block code
+     * <code>false</code> when unsuccessfully run this block code
+     */
+    @Override
+    protected boolean resolve() {
+        Socket socket = FakeAuthentication.getInstance().getSocket();
+        try{
+            socket.write(chain.getProcessObject().toString());
+        } catch (IOException e){
+            System.err.println("Cannot write to data stream");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
