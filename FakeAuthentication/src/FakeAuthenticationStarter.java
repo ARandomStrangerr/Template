84a0212e08ac483e93f554bd7ba9bdefea8b6337
thanhdo <billslim0996@gmail.com>
2022-03-27 01:49:31 -0700

    add starter class

---
+++ FakeAuthentication/src/FakeAuthenticationStarter.java
@@ -0,0 +1,13 @@
+import chain.fake_authentication.init.InitChain;
+
+import java.net.InetAddress;
+
+public class FakeAuthenticationStarter {
+    public static void main(String[] args) throws Exception{
+        new InitChain(InetAddress.getByName("localhost"),
+                9999,
+                "FakeAuthentication",
+                "authentication.txt")
+                .resolve();
+    }
+}
