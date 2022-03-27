f5c40c42e65a1fd333b90b1999b845d8ad9a00bc
thanhdo <billslim0996@gmail.com>
2022-03-27 01:19:35 -0700

    check the input json format to see if it is in the correct format.

--- FakeAuthentication/src/chain/fake_authentication/resolve/LinkCheckJson.java
+++ FakeAuthentication/src/chain/fake_authentication/resolve/LinkCheckJson.java
@@ -17,8 +17,32 @@ class LinkCheckJson extends Link<ResolveChain> {
      @Override
      protected boolean resolve() {
          JsonObject body = chain.getProcessObject().get("body").getAsJsonObject();
+         //check the client id
          if (!body.has("clientId")) {
-
+             System.err.println("the body does not contain name of the client");
+             chain.getProcessObject().addProperty("error", "body does not contain name of the client");
+             return false;
+         }
+         try{
+             body.get("clientId").getAsString();
+         } catch (Exception e) {
+             System.err.println("the client id can not be converted to string");
+             e.printStackTrace();
+             chain.getProcessObject().addProperty("error", "client id can not be converted to string");
+             return false;
+         }
+         // check the requested module
+         if (!body.has("requestModule")) {
+             System.err.println("the body does nto contain list of request modules");
+             chain.getProcessObject().addProperty("error", "body does not contain list of request modules");
+             return false;
+         }
+         try {
+             body.get("requestModule").getAsJsonArray();
+         } catch (Exception e){
+             System.err.println("the requestModule is not in correct form");
+             e.printStackTrace();
+             chain.getProcessObject().addProperty("error", "the requestModule is not in correct format");
              return false;
          }
          return true;
