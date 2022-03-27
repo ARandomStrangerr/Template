fe145baf42a41b757318f8accef6c8da18986379
thanhdo <billslim0996@gmail.com>
2022-03-27 01:20:35 -0700

    chain class to chain responsibility

--- FakeAuthentication/src/chain/fake_authentication/resolve/ResolveChain.java
+++ FakeAuthentication/src/chain/fake_authentication/resolve/ResolveChain.java
@@ -10,6 +10,7 @@ public class ResolveChain extends Chain {
 
     @Override
     protected void chainConstruction() {
-
+        super.chain.add(new LinkCheckPrivilege(this));
+        super.chain.add(new LinkCheckPrivilege(this));
     }
 }
