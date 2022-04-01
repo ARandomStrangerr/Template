7036825618ad529daa7468650ac7171f66dbc687
thanhdo <billslim0996@gmail.com>
2022-03-31 21:18:43 -0700

    starter class and test class for IncomingConnectionStarter.java

--- IncommingConnection/src/IncomingConnectionStarter.java
+++ IncommingConnection/src/IncomingConnectionStarter.java
@@ -1,4 +1,5 @@
-import chain.incoming_connection.InitChain;
+
+import chain.incoming_connection.init.InitChain;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,22 +10,27 @@ import java.net.Socket;
 
 public class IncomingConnectionStarter {
     public static void main(String[] args) throws Exception{
-        new InitChain(
+        new InitChain(InetAddress.getByName("localhost"),
+                9999,
+                10000,
                 "IncomingConnection",
-                InetAddress.getByName("localhost"),
-                1998,
-                1997
-        ).resolve();
+                3000)
+                .resolve();
     }
 }
 
 class Main1 {
     public static void main(String[] args) throws Exception{
-        Socket socket = new Socket(InetAddress.getByName("localhost"), 1997);
+        Socket socket = new Socket(InetAddress.getByName("localhost"), 10000);
         BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        bw.write("{job:\"Authentication\", id:\"habcd\", data:\"hello\"}");
+        bw.write("UNKNOWN_MAC_ADDRESS");
+        bw.newLine();
+        bw.flush();
+
+        String send = "{job: \"GetInvoice\"}";
+        bw.write(send);
         bw.newLine();
         bw.flush();
 
