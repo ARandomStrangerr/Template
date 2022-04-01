86110353aaffd5056aeefbc5efc934792ee94c48
thanhdo <billslim0996@gmail.com>
2022-03-31 21:15:20 -0700

    allow the chain to end without reaching to the end and still return true

--- ShareResource/src/chain/Chain.java
+++ ShareResource/src/chain/Chain.java
@@ -2,16 +2,19 @@ package chain;
 
 import com.google.gson.JsonObject;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Chain {
     private final JsonObject processObject;
     protected final List<Link> chain;
+    private boolean resolveEarly;
 
     public Chain(JsonObject processObject) {
         chain = new LinkedList<>();
         this.processObject = processObject;
+        this.resolveEarly = false;
         chainConstruction();
     }
 
@@ -22,7 +25,9 @@ public abstract class Chain {
      * false when the chain broke midway
      */
     public final boolean resolve() {
-        for (Link link : chain) {
+        Iterator<Link> iterator = chain.iterator();
+        while (iterator.hasNext() && !resolveEarly) {
+            Link link = iterator.next();
             if (!link.resolve()) return false;
         }
         return true;
@@ -33,6 +38,13 @@ public abstract class Chain {
      */
     public final JsonObject getProcessObject() {
         return processObject;
+    }
+
+    /**
+     * mark the chain to end early and return true without reaching to the end of its chain.
+     */
+    public final void resolveEarly(){
+        resolveEarly = true;
     }
 
     protected abstract void chainConstruction();
