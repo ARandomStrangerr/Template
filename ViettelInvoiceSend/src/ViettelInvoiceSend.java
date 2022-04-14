a8ab3e403446dd05a2accc9c33f7d7ce8afe8ce6
thanhdo <billslim0996@gmail.com>
2022-04-14 15:40:52 -0700

    update the starter method according to the new class

--- ViettelInvoiceSend/src/ViettelInvoiceSend.java
+++ ViettelInvoiceSend/src/ViettelInvoiceSend.java
@@ -1,11 +1,11 @@
-import chain.viettel_invoice_send.InitChain;
+import chain.viettel_invoice_send.init.InitChain;
 
 import java.net.InetAddress;
 
 public class ViettelInvoiceSend {
     public static void main(String[] args) throws Exception {
-        new InitChain("ViettelInvoiceSend",
-                InetAddress.getByName("localhost"),
+        new InitChain(InetAddress.getByName("localhost"),
+                "ViettelInvoiceSend",
                 9999)
                 .resolve();
     }
