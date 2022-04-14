8c605cce0d17eb8d9650914c3b8400ed6baf0a99
thanhdo <billslim0996@gmail.com>
2022-04-14 15:41:37 -0700

    include links need to run the chain

--- ViettelInvoiceSend/src/chain/viettel_invoice_send/resolve/ResolveChain.java
+++ ViettelInvoiceSend/src/chain/viettel_invoice_send/resolve/ResolveChain.java
@@ -1,15 +1,21 @@
 package chain.viettel_invoice_send.resolve;
 
 import chain.Chain;
+import chain.viettel_invoice_send.LinkSendToDataStream;
 import com.google.gson.JsonObject;
 
+import java.io.File;
+
 public class ResolveChain extends Chain {
+    File excelFile;
     public ResolveChain(JsonObject processObject) {
         super(processObject);
     }
 
     @Override
     protected void chainConstruction() {
-
+        chain.add(new LinkMaterializeExcelFile(this));
+        chain.add(new LinkReadExcelFile(this));
+        chain.add(new LinkSendToDataStream(this));
     }
 }
