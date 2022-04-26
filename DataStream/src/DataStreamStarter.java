8a4aa5f58ae85e8694e53d349a96e9aa55cbe602
Thanh Do <ttd9@lib-3000-m12.ucslabs.sfu.ca>
2022-04-26 15:11:27 -0700

    port and timeout is passed as arg

--- DataStream/src/DataStreamStarter.java
+++ DataStream/src/DataStreamStarter.java
@@ -2,6 +2,19 @@ import chain.data_stream.init_module.InitChain;
 
 public class DataStreamStarter {
     public static void main(String[] args) {
-        new InitChain(9999, 3000).resolve();
+        int port, timeout;
+        try {
+            port = Integer.parseInt(args[0]);
+            timeout = Integer.parseInt(args[1]);
+        } catch (IndexOutOfBoundsException e){
+            System.err.println("missing passing in program argument");
+            e.printStackTrace();
+            return;
+        } catch (NumberFormatException e){
+            System.err.println("passing in number is in correct format");
+            e.printStackTrace();
+            return;
+        }
+        new InitChain(port, timeout).resolve();
     }
 }
